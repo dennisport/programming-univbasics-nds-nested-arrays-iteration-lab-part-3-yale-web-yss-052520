fdd1ea430cc065ab52609751a087310c2902c0d3
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 15:42:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,9 @@ mixed_data_statement =[]
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
+    if src[row_index][element_index]
     mixed_data_statement << src[row_index][element_index]
+    
     element_index += 1
   end
   row_index += 1
